a6a87f1857a3f07a1f3eb1fdc5a9aaf6bf9d0f54
云影 <1220998011@qq.com>
2021-09-12 10:10:05 +0800

    separate ast and virtual dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import h from "./h.js";
+import a from "./a";
+import patch from "./patch";
+let container = document.getElementById("app");
 
 let data = `{"a":1,"b":{"c":2,"d":[3,"4",{"e":"5","f":[],"g":{}}]}}`;
 data = JSON.parse(data);
-console.log(h(0, "", data));
+let astTree = a(0, "", data);
+console.log(astTree);
+
+astTree.value.map((item)=>{
+    
+});
+
+// patch(container, vNode);
---
+++ src/patch.js
@@ -0,0 +1,4 @@
+function patch(container, vNode){
+    console.log(vNode);
+}
+export default patch;
